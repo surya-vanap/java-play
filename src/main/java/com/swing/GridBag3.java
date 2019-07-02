74c5567700e3e351b960d2c96b315d61573b8840
Surya Vanaparthy <surya.vanaparthy@halliburton.com>
2019-07-02 11:01:41 -0500

    Updating changes

--- src/main/java/com/swing/GridBag3.java
+++ src/main/java/com/swing/GridBag3.java
@@ -1,37 +1,77 @@
 package com.swing;
 
-//file: GridBag4.java
 import java.awt.BorderLayout;
-import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JSlider;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 public class GridBag3 extends JPanel {
-	GridBagConstraints constraints = new GridBagConstraints();
+	private static final long serialVersionUID = 2957839971021687243L;
 	private final JPanel _panelToolBar = new JPanel();
 	private final JPanel _panelIntervals = new JPanel();
 
 	public GridBag3() {
-		setLayout(new GridBagLayout());
+		// setMinimumSize(new Dimension(200, 300));
+		// setPreferredSize(new Dimension(300, 400));
+		GridBagLayout gbl_mainPanel = new GridBagLayout();
+		gbl_mainPanel.columnWidths = new int[] { 200 };
+		gbl_mainPanel.rowHeights = new int[] { 0, 200 };
+		gbl_mainPanel.columnWeights = new double[] { 1.0 };
+		gbl_mainPanel.rowWeights = new double[] { 0.0, 1.0 };
+		setLayout(gbl_mainPanel);
 
-		add(_panelToolBar, new GridBagConstraints(0, 0, 1, 1, 1.0, 0.0, GridBagConstraints.WEST,
+		add(_panelToolBar, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0, GridBagConstraints.NORTHWEST,
 				GridBagConstraints.HORIZONTAL, new Insets(0, 0, 0, 0), 0, 0));
 
 		_panelToolBar.setLayout(new BorderLayout(0, 0));
+		JButton btn = new JButton("one");
+		JCheckBox cb = new JCheckBox();
+		cb.addItemListener(new ItemListener() {
 
-		_panelToolBar.add(new JButton("one"), BorderLayout.WEST);
-		_panelToolBar.add(new JButton("to"), BorderLayout.EAST);
+			@Override
+			public void itemStateChanged(ItemEvent e) {
+				if (e.getStateChange() == ItemEvent.SELECTED) {
+					System.out.println("HERE");
+				}
 
-		add(_panelIntervals, new GridBagConstraints(0, 1, 1, 1, 1.0, 1.0, GridBagConstraints.NORTHWEST,
-				GridBagConstraints.HORIZONTAL, new Insets(0, 0, 0, 0), 0, 0));
+			}
+		});
+		cb.addChangeListener(new ChangeListener() {
+
+			@Override
+			public void stateChanged(ChangeEvent e) {
+
+				System.out.println("HERE");
+			}
+		});
+		btn.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				cb.setSelected(!cb.isSelected());
+
+			}
+		});
+		_panelToolBar.add(btn, BorderLayout.WEST);
+		_panelToolBar.add(cb, BorderLayout.EAST);
+
+		add(_panelIntervals, new GridBagConstraints(0, 1, 1, 1, 0.0, 0.0, GridBagConstraints.NORTHWEST,
+				GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
 		GridBagLayout gbl_algorithmParmsPanel = new GridBagLayout();
 
 		// gbl_algorithmParmsPanel.columnWidths = new int[] { 100, 50, 25};
@@ -44,62 +84,58 @@ public class GridBag3 extends JPanel {
 		_panelIntervals.setLayout(gbl_algorithmParmsPanel);
 
 		addParam(true, "min", 0);
-		addParam(true, "max", 1);
+		addParam(false, "max", 1);
 	}
 
 	private void addParam(boolean showCheck, String message, int gridy) {
 		Dimension labelDimension = new Dimension(100, 24);
-		Dimension valueDimension = new Dimension(75, 24);
-		Dimension unitDimension = new Dimension(25, 24);
+		Dimension valueDimension = new Dimension(200, 24);
+		Dimension unitDimension = new Dimension(200, 24);
 
 		JLabel param = new JLabel(message);
-
-		_panelIntervals.add(param, new GridBagConstraints(0, gridy, 1, 1, 0.0, 0.0, GridBagConstraints.FIRST_LINE_START,
+		param.setPreferredSize(labelDimension);
+		param.setMinimumSize(labelDimension);
+		_panelIntervals.add(param, new GridBagConstraints(0, gridy, 1, 1, 0.7, 0.0, GridBagConstraints.FIRST_LINE_START,
 				GridBagConstraints.HORIZONTAL, new Insets(10, 0, 0, 0), 0, 0));
 
 		if (showCheck) {
-			JSlider _valueSlider = new JSlider();
-			_valueSlider.setMinimum(1);
-			_valueSlider.setMaximum(10);
-			_valueSlider.setSnapToTicks(true);
-			_valueSlider.setValue(4);
-			_valueSlider.setMajorTickSpacing(1);
-			_valueSlider.setMinorTickSpacing(1);
-			_valueSlider.setPaintTicks(true);
-			_valueSlider.setPaintLabels(true);
-
-			_panelIntervals.add(_valueSlider,
-					new GridBagConstraints(1, gridy, 1, 1, 0.0, 0.0, GridBagConstraints.FIRST_LINE_START,
+			JSlider valueSlider = new JSlider();
+			valueSlider.setMinimum(1);
+			valueSlider.setMaximum(10);
+			valueSlider.setSnapToTicks(true);
+			valueSlider.setValue(4);
+			valueSlider.setMajorTickSpacing(1);
+			valueSlider.setMinorTickSpacing(1);
+			valueSlider.setPaintTicks(true);
+			valueSlider.setPaintLabels(true);
+			valueSlider.setPreferredSize(valueDimension);
+			valueSlider.setMinimumSize(valueDimension);
+			_panelIntervals.add(valueSlider,
+					new GridBagConstraints(1, gridy, 1, 1, 0.3, 0.0, GridBagConstraints.FIRST_LINE_START,
+							GridBagConstraints.HORIZONTAL, new Insets(0, 10, 0, 10), 0, 0));
+		} else {
+			JCheckBox cb = new JCheckBox();
+			cb.setMinimumSize(unitDimension);
+			_panelIntervals.add(cb,
+					new GridBagConstraints(1, gridy, 1, 1, 0.3, 0.0, GridBagConstraints.FIRST_LINE_START,
 							GridBagConstraints.HORIZONTAL, new Insets(0, 10, 0, 10), 0, 0));
 		}
-
-		JLabel param2 = new JLabel("");
-
-		_panelIntervals.add(param2, new GridBagConstraints(0, gridy, 1, 1, 1.0, 0.0,
-				GridBagConstraints.FIRST_LINE_START, GridBagConstraints.HORIZONTAL, new Insets(10, 0, 0, 0), 0, 0));
-	}
-
-	public class GridBag4 extends JPanel {
-		public GridBag4() {
-			JPanel panel = new GridBag4();
-			// _conformanceQQSPanel.setVisible(false);
-			add(panel, new GridBagConstraints(0, 0, 1, 1, 1.0, 1.0, GridBagConstraints.WEST, GridBagConstraints.BOTH,
-					new Insets(0, 0, 0, 0), 0, 0));
-		}
-	}
-
-	void addGB(Component component, int x, int y) {
-		constraints.gridx = x;
-		constraints.gridy = y;
-		add(component, constraints);
 	}
 
 	public static void main(String[] args) {
 		JFrame frame = new JFrame("GridBag4");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(300, 100);
-		frame.setLocation(200, 200);
-		frame.setContentPane(new GridBag3());
+		// frame.setSize(300, 100);
+		// frame.setLocation(200, 200);
+		JScrollPane scrollPaneGridDefPanel = new JScrollPane();
+		scrollPaneGridDefPanel.setViewportBorder(null);
+		// scrollPaneGridDefPanel.setPreferredSize(new Dimension(630, 300));
+		// scrollPaneGridDefPanel.setMinimumSize(new Dimension(385, 240));
+
+		scrollPaneGridDefPanel.setViewportView(new GridBag3());
+
+		frame.setContentPane(scrollPaneGridDefPanel);
 		frame.setVisible(true);
+		frame.pack();
 	}
 }
--- src/main/java/com/swing/SliderTest.java
+++ src/main/java/com/swing/SliderTest.java
@@ -16,7 +16,7 @@ public class SliderTest {
 		final DecimalFormat df = new DecimalFormat("0.########");
 		final JFrame frame = new JFrame();
 		final JTextField text = new JTextField(20);
-		final DoubleJSlider slider = new DoubleJSlider(0, 100, 0, 100);
+		final DoubleJSlider slider = new DoubleJSlider(0, 100, 0, 100, 10);
 		slider.addChangeListener(new ChangeListener() {
 			@Override
 			public void stateChanged(ChangeEvent e) {
@@ -27,11 +27,11 @@ public class SliderTest {
 			@Override
 			public void keyReleased(KeyEvent ke) {
 				String typed = text.getText();
-				slider.setValue(0);
-				if (!typed.matches("\\d+(\\.\\d*)?")) {
-					return;
-				}
-				double value = Double.parseDouble(typed) * slider.scale;
+				// slider.setValue(0);
+				// if (!typed.matches("\\d+(\\.\\d*)?")) {
+				// return;
+				// }
+				double value = (Double.parseDouble(typed) / 10.0) * slider.scale;
 				slider.setValue((int) value);
 			}
 		});
@@ -49,13 +49,15 @@ public class SliderTest {
 class DoubleJSlider extends JSlider {
 	private static final long serialVersionUID = 1L;
 	final int scale;
+	private double _maxValue;
 
-	public DoubleJSlider(int min, int max, int value, int scale) {
+	public DoubleJSlider(int min, int max, int value, int scale, double maxValue) {
 		super(min, max, value);
 		this.scale = scale;
+		_maxValue = maxValue;
 	}
 
 	public double getScaledValue() {
-		return ((double) super.getValue()) / this.scale;
+		return ((double) super.getValue() * _maxValue) / this.scale;
 	}
 }
